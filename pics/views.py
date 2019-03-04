fa54c9e0f82f5ee83eec826c6cfe0f919f8ad71a
Albert Omware <aomware@gmail.com>
2019-03-04 19:31:51 +0300

    added the views to the app

--- pics/views.py
+++ pics/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404
+import datetime as dt
+from .models import Image,Category,Location
 # Create your views here.
-def intro(request):
-    return render (request,'first.html')
 
+def photos(request):
+    images = Image.get_photos()
+    return render(request,'index.html',{"images":images})
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        category = request.GET.get("image")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any image"
+        return render(request, 'search.html',{"message":message})    
+def image(request,image_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"image.html", {"image":image})
+def location(request,location):
+        locations = Image.filter_by_location(location)
+        return render(request,'location.html',{"images": locations})
+
+def page(request):
+    return render(request,"page.html",{"title":location})
