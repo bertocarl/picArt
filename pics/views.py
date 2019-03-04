f38cb70c3e92f274f9df6b12ea03852f7b4a8703
Albert Omware <aomware@gmail.com>
2019-03-04 20:33:06 +0300

    made changes to my search

--- pics/views.py
+++ pics/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
 from .models import Image,Category,Location
-# Create your views here.
+
 
 def photos(request):
     images = Image.get_photos()
