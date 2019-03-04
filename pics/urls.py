4debff920a0bba6978b62000ef137fa9181d4091
Albert Omware <aomware@gmail.com>
2019-03-04 19:34:24 +0300

    added the classes to the admin panel

--- pics/urls.py
+++ pics/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.intro,name='intro'),
+    url(r'^$',views.photos,name='photos'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r"^page/",views.page, name = "location"),
+    url(r"^location/(\w+)",views.location, name = "locations")
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
