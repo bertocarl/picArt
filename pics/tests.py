e055e1dc9eb20524f232841919375791b6d14722
Albert Omware <aomware@gmail.com>
2019-03-05 06:55:06 +0300

    added image test

--- pics/tests.py
+++ pics/tests.py
@@ -4,7 +4,12 @@ from .models import Location,Category,Image
 
 class LocationTestclass(TestCase):
     def test_save_method(self):
-        self.rwanda.save_location()
+        self.location.save_location()
         locations = Location.objects.all()
         self.assertTrue(len(locations) > 0)
 
+class ImageTestclass(TestCase):
+    def test_save_method(self):
+        self.image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
